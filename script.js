27d566d09dc5f0974d210f8e91d60b0a236911e8
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-05-10 17:57:35 +0100

    added ajax requests to js, html user input start

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-$.ajax({
-  type: 'GET',
-  url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=191',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: JSON.stringify({
-    task: {
-      content: 'Feed the plants'
+$(document).ready(function(){
+  
+  $.ajax({
+    type: 'DELETE',
+    url: 'https://fewd-todolist-api.onrender.com/tasks/3115?api_key=191',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
     }
-  }),
-  success: function (response, textStatus) {
-    console.log(response);
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
+  });
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=191',
+    dataType: 'json', 
+    success: function (response, textStatus) {
+      console.log(response);
+      response.tasks.forEach(function (task) {
+        $('#to-do-list').append('<p>' + task.content + '</p>');
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 });
